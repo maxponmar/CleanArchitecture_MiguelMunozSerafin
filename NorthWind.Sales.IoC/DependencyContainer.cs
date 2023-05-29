b7e4f67b4b312054fc75b7b046cbc9b90bba4fa3
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2023-05-28 21:10:57 -0700

    Se crea proyecto IoC para facilitar la inyeccion de dependencias

---
+++ NorthWind.Sales.IoC/DependencyContainer.cs
@@ -0,0 +1,18 @@
+﻿namespace NorthWind.Sales.IoC
+{
+    public static class DependencyContainer
+    {
+        public static IServiceCollection AddNorthWindSalesServices(
+            this IServiceCollection services,
+            IConfiguration configuration,
+            string connectionStringName)
+        {
+            services.AddRepositories(configuration, connectionStringName);
+            services.AddUseCasesServices();
+            services.AddPresenters();
+            services.AddNorthWindSalesControllers();
+
+            return services;
+        }
+    }
+}
