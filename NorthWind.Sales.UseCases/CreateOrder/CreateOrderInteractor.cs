423f4e69304440c98085351b915182de6c5dc5d3
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2023-05-28 19:22:47 -0700

    Se crea interactor para crear orden

---
+++ NorthWind.Sales.UseCases/CreateOrder/CreateOrderInteractor.cs
@@ -0,0 +1,23 @@
+﻿namespace NorthWind.Sales.UseCases.CreateOrder
+{
+    public class CreateOrderInteractor : ICreateOrderInputPort
+    {
+        readonly ICreateOrderOutputPort OutputPort;
+        readonly INorthWindSalesCommandsRepository Repository;
+
+        public CreateOrderInteractor(ICreateOrderOutputPort outputPort, INorthWindSalesCommandsRepository repository)
+        {
+            OutputPort = outputPort;
+            Repository = repository;
+        }
+
+        public async ValueTask Handle(CreateOrderDTO orderDTO)
+        {
+            OrderAggregate Order = OrderAggregate.From(orderDTO);
+
+            await Repository.CreateOrder(Order);
+            await Repository.SaveChanges();
+            await OutputPort.Handle(Order.Id);
+        }
+    }
+}
