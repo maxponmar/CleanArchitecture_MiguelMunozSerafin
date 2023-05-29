c810ab3020e9096c2eeec1b916ecc7fdea731816
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2023-05-28 19:23:02 -0700

    Se agrega configuracion de dependencias

---
+++ NorthWind.Sales.UseCases/DependencyContainer.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NorthWind.Sales.UseCases.CreateOrder;
+
+namespace NorthWind.Sales.UseCases
+{
+    public static class DependencyContainer
+    {
+        public static IServiceCollection AddUseCasesServices(this IServiceCollection services)
+        {
+            services.AddScoped<ICreateOrderInputPort, CreateOrderInteractor>();
+            return services;
+        }
+    }
+}
---
+++ NorthWind.Sales.UseCases/_Imports.cs
@@ -0,0 +1,4 @@
+﻿global using NorthWind.Sales.BusinessObjects.Aggregates;
+global using NorthWind.Sales.BusinessObjects.DTOs.CreateOrder;
+global using NorthWind.Sales.BusinessObjects.Interfaces.Ports;
+global using NorthWind.Sales.BusinessObjects.Repositories;
