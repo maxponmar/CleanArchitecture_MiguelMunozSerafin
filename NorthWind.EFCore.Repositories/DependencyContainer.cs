7553a5f38f66192c4742314e05de832d69b151a1
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2023-05-28 20:14:44 -0700

    Se agrega configuracion de dependencias para repositorio y dbcontext

---
+++ NorthWind.EFCore.Repositories/DependencyContainer.cs
@@ -0,0 +1,20 @@
+﻿namespace NorthWind.EFCore.Repositories
+{
+    public static class DependencyContainer
+    {
+        public static IServiceCollection AddRepositories(
+            this IServiceCollection services,
+            IConfiguration configuration,
+            string connectionStringName)
+        {
+            services.AddDbContext<NorthWindSalesContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString(connectionStringName));
+            });
+
+            services.AddScoped<INorthWindSalesCommandsRepository, NorthWindSalesCommandsRepository>();
+
+            return services;
+        }
+    }
+}
