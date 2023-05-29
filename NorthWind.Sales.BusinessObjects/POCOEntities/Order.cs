608c211dabb651f5f472ede3e871fc041f147a44
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2023-05-28 19:22:35 -0700

    Se agrega propiedad faltante

--- NorthWind.Sales.BusinessObjects/POCOEntities/Order.cs
+++ NorthWind.Sales.BusinessObjects/POCOEntities/Order.cs
@@ -7,6 +7,7 @@
         public string ShipAddress { get; set; }
         public string ShipCity { get; set; }
         public string ShipCountry { get; set; }
+        public string ShipPostalCode { get; set; }
         public ShippingType ShippingType { get; set; } = ShippingType.Road;
         public DiscountType DiscountType { get; set; } = DiscountType.Percentage;
         public double Discount { get; set; } = 10;
