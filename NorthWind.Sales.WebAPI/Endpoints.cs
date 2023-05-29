db8376c572700d2c12afc0574cc264930abe899a
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2023-05-28 21:11:12 -0700

    Se crea API

---
+++ NorthWind.Sales.WebAPI/Endpoints.cs
@@ -0,0 +1,12 @@
+﻿namespace NorthWind.Sales.WebAPI
+{
+    public static class Endpoints
+    {
+        public static WebApplication UseNorthWindSalesEndpoints(this WebApplication app)
+        {
+            app.MapPost("/create", async (CreateOrderDTO order, ICreateOrderController controller) => Results.Ok(await controller.CreateOrder(order)));
+
+            return app;
+        }
+    }
+}
---
+++ NorthWind.Sales.WebAPI/WebApplicationHelper.cs
@@ -0,0 +1,28 @@
+﻿namespace NorthWind.Sales.WebAPI
+{
+    public static class WebApplicationHelper
+    {
+        public static WebApplication CreateWebApplication(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddNorthWindSalesServices(builder.Configuration, "NorthWindConnectionString");
+
+            return builder.Build();
+        }
+
+        public static WebApplication ConfigureWebApplication(this WebApplication app)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseNorthWindSalesEndpoints();
+
+            return app;
+        }
+    }
+}
